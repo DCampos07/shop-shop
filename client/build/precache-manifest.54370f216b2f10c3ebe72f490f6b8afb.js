e06b2c2220de9931e67271559f0f2595f1ae72de
Damaris C <DCampos07@users.noreply.github.com>
2021-01-24 18:47:17 -0500

    fixing react bug

---
+++ client/build/precache-manifest.54370f216b2f10c3ebe72f490f6b8afb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10485554b4ba8bb3c7d674ca53f8fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cdf25ac1281002be3c2",
+    "url": "/static/css/main.19d4db8e.chunk.css"
+  },
+  {
+    "revision": "0d6ce740c266e167fe0b",
+    "url": "/static/js/2.7f36acf9.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.7f36acf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cdf25ac1281002be3c2",
+    "url": "/static/js/main.a1e223c7.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
