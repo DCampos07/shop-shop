e1308b904c96d8a18a000ce2446d35e176146ec9
Damaris C <DCampos07@users.noreply.github.com>
2021-01-25 13:50:52 -0500

    checked spelling

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -34,7 +34,7 @@ function Success() {
     <div>
       <Jumbotron>
         <h1>Success!</h1>
-        <h2>Thank youi for your purchase!</h2>
+        <h2>Thank you for your purchase!</h2>
         <h2>You will now be redirected to the homepage</h2>
       </Jumbotron>
     </div>
